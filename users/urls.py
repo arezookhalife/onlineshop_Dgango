54aab565f1a7e49eaea7b3c493a10c24fd3b4d5f
arezoo <arezoo.kh1988@gmail.com>
2025-08-17 12:11:33 +0330

    DAY 6: add protected GET API for authenticated users

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegisterView, UserUpdateView
+from .views import RegisterView, UserUpdateView, protected_view
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/<int:pk>/', UserUpdateView.as_view(), name='update-profile'),
+    path('protected/', protected_view, name='protected'),
 ]
